9fabc5f1a521764c84460c5847264822d005676c
DANNJUGUNA <ndan30071@gmail.com>
2023-02-04 10:28:02 +0300

    created the structure of the components

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./rightbar.css"
+const Rightbar = () => {
+  return (
+    <div>Rightbar</div>
+  )
+}
+
+export default Rightbar
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./sidebar.css"
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
