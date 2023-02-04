4a634fdb530bced4d4050f378cef5a2198ef49d0
DANNJUGUNA <ndan30071@gmail.com>
2023-02-04 10:38:51 +0300

    connected other components and styled the home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import TopBar from '../../components/topBar/TopBar'
-
+import "./home.css"
 const Home = () => {
   return (
+    <>
     <TopBar/>
+    <div className="homeContainer">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+    </div>
+    </>
   )
 }
 
