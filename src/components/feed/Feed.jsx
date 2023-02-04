5e093b30d143bcb58eec61220c35463c570a81be
DANNJUGUNA <ndan30071@gmail.com>
2023-02-05 00:53:52 +0300

    designed and styled the feed component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Share from '../share/Share'
+import "./feed.css"
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='feed'>
+        <div className="feedwrapper">
+          <Share/>
+        </div>
+    </div>
   )
 }
 
