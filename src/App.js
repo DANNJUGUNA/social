264931f534142bec8a56d3d6f76f8462eb63eebc
DANNJUGUNA <ndan30071@gmail.com>
2023-02-04 03:27:38 +0300

    linked home page to the app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
-
+import React from "react";
+import Home from "./pages/home/Home";
 function App() {
   return (
-    <div></div>
-  );
+    <div>
+      <Home/>
+    </div>
+  )
+  
 }
 
 export default App;
