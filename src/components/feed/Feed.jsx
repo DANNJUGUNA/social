e9c77d09c80b961ceae24f62f008af1b1d1770b4
DANNJUGUNA <ndan30071@gmail.com>
2023-02-05 09:30:17 +0300

    Updated the feed design

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from '../posts/Post'
 import Share from '../share/Share'
 import "./feed.css"
 const Feed = () => {
@@ -6,6 +7,9 @@ const Feed = () => {
     <div className='feed'>
         <div className="feedwrapper">
           <Share/>
+          <Post/>
+          <Post/>
+          <Post/>
         </div>
     </div>
   )
