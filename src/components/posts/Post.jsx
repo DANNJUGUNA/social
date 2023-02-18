fe5f7033c5dd49ab9ad60af25731546eabf61fe9
DANNJUGUNA <ndan30071@gmail.com>
2023-02-18 09:24:23 +0300

    added functionality of the like button

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./post.css"
+import {Users} from "../../data";
 import { MoreVert } from '@mui/icons-material'
-const Post = () => {
+const Post = ({post}) => {
+  let photo=post.photo
+const [like,setLike]=useState(post.like)
+const [isLiked,setIsLiked]=useState(false)
+
+const   handleLike=()=>{
+setLike(isLiked? like-1 :like+1)
+setIsLiked(!isLiked)
+}
   return (
     <div className='post'>
        <div className="postwrapper">
         <div className="top">
             <div className="topleft">
-                <img className='postprofileimg' src="./assets/person/1.jpeg" alt="picture" />
-                <span className="username">Dan Njuguna</span>
-                <span className="postDate">5 mins ago</span>
+                <img className='postprofileimg' 
+                src={Users.filter((user)=>user.id===post.userId)[0]. profilePicture} 
+                alt="picture" />
+                <span className="username">
+                  {Users.filter((user)=>user.id===post.userId)[0].username}
+                  </span>
+                <span className="postDate">{post.date}</span>
             </div>
             <div className="topright">
               <MoreVert/>
@@ -17,18 +30,18 @@ const Post = () => {
         </div>
         <div className="center">
           <span className="posttext">
-            Hey! It's my first post. I am very greatfull
+            {post.desc}
           </span>
-          <img className='postimg' src="/assets/posts/1.jpeg" alt="post" />
+          <img className='postimg' src={photo} alt="post" />
         </div>
         <div className="bottom">
           <div className="bottomleft">
-            <img className='likeicon' src="/assets/heart.png" alt="heart" />
-            <img className='likeicon' src="/assets/like.png" alt="heart" />
-            <span className="postlikecounter">23 people liked it</span>
+            <img className='likeicon' src="/assets/heart.png" onClick={handleLike}alt="heart" />
+            <img className='likeicon' src="/assets/like.png" onClick={handleLike} alt="like" />
+            <span className="postlikecounter">{like} people liked it</span>
           </div>
           <div className="bottomright">
-            <span className="postcoments">9 comments</span>
+            <span className="postcoments">{post.comment} comments</span>
           </div>
         </div>
        </div>
